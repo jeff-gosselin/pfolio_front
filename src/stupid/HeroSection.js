383d74202648390a9082bd74b4f2dd000d7da22f
jeff-gosselin <jgoz.email@gmail.com>
2019-08-05 19:12:15 -0400

    changed top nav to flexbox

--- src/stupid/HeroSection.js
+++ src/stupid/HeroSection.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import TopNav from './TopNav';
 import '../css/HeroSection.scss';
-import logo from '../img/jg-logo.svg';
 import man from '../img/man-on-mouse.png';
 
 const HeroSection = () => {
     return (
         <section id="hero">
-            <div id="logo">
-                <img src={logo} alt="Jeff Gosselin: Web Developer / Designer"/>
-                <h1><span>Jeff</span> Gosselin</h1>
-            </div>
             <TopNav/>
             <div class="man-on-mouse">
                 <img src={man} alt="Jeff"/>
--- src/stupid/TopNav.js
+++ src/stupid/TopNav.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import logo from '../img/jg-logo.svg';
 import '../css/TopNav.scss';
 
 const TopNav = () => {
     return (
         <nav>
-            <ul>
+            <div id="logo">
+                <img src={logo} alt="Jeff Gosselin: Web Developer / Designer"/>
+                <h1><span>Jeff</span> Gosselin</h1>
+            </div>
+            <ul id="pages">
                 <li>Bio</li>
                 <li>Work</li>
                 <li>Resume</li>
