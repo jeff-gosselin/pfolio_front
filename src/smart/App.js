bc2343718074acb3ec61d04877011f10bafad1bc
jeff-gosselin <jgoz.email@gmail.com>
2019-08-05 19:31:21 -0400

    added bio section

--- src/smart/App.js
+++ src/smart/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/App.scss';
 import HeroSection from '../stupid/HeroSection';
+import BioSection from '../stupid/BioSection';
 
 function App() {
   return (
     <div>
-      <HeroSection/>
+      <HeroSection />
+      <BioSection />
     </div>
   );
 };
---
+++ src/stupid/BioSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../css/BioSection.scss';
+
+const BioSection = () => {
+    return (
+        <section id="bio">
+            <h1>This is the Bio Section</h1>
+        </section>
+    );
+};
+export default BioSection;
