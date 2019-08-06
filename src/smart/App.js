94548a0cbfec558b537e33bc5f43aa7b0325e8ff
jeff-gosselin <jgoz.email@gmail.com>
2019-08-05 21:00:51 -0400

    added work section

--- src/smart/App.js
+++ src/smart/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../css/App.scss';
 import HeroSection from '../stupid/HeroSection';
 import BioSection from '../stupid/BioSection';
+import WorkSection from '../stupid/WorkSection';
 
 function App() {
   return (
     <div>
       <HeroSection />
       <BioSection />
+      <WorkSection />
     </div>
   );
 };
---
+++ src/stupid/WorkSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../css/WorkSection.scss';
+
+const WorkSection = () => {
+    return (
+        <section id="work">
+            <h1>This is the Work Section</h1>
+        </section>
+    );
+};
+export default WorkSection;
