a02e7e4f99b38a58c06187444abcbc9f8f8af656
jeff-gosselin <jgoz.email@gmail.com>
2019-09-25 23:27:52 -0400

    contact form is working

--- src/client/sections/ContactSection.js
+++ src/client/sections/ContactSection.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import '../../css/client_css/ContactSection.scss';
 
 class ContactSection extends Component {
-    handleInput = () => {
 
+    state = {
+        name: '',
+        email: '',
+        message: '',
+        contactID: Math.random() * 100000 | 0
     }
 
-    handleInput = () => {
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })      
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        var templateParams = {
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message,
+            contactID: this.state.contactID
+        };
+         
+        window.emailjs.send('gmail', 'contact_form', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
         
+     
     }
     render() {
+        console.log(this.state);
         return (
             <section id="contact">
                 <div className="contact-content">
                         <h1>Contact Me</h1>
-                        <form onSubmit={this.handleSubmit}>
+                        <form id="contact-form" onSubmit={this.handleSubmit}>
                             <input name="name" type="text" placeholder="Name" onChange={this.handleInput}/>
                             <input name="email" type="email" placeholder="Email" onChange={this.handleInput}/>
                             <textarea name="message" type="text" placeholder="Message" onChange={this.handleInput}/>
