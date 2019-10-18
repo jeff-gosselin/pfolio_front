f664693c1ee33787f48f1e93acff78fec86422b0
jeff-gosselin <jgoz.email@gmail.com>
2019-10-18 16:00:31 -0400

    added animation to skill level

--- src/client/components/Skill.js
+++ src/client/components/Skill.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../../css/client_css/Skill.scss';
 
-// icons
-import { FaGithub } from 'react-icons/fa';
+class Skill extends Component {
+    state = {
+        divStyle: {}
+    }
 
-const Skill = (props) => {
-    console.log("props: ", props.skill.name);
-    const {name, level} = props.skill;
-    const barLength = `${level}%`;
-    const divStyle = {
-        height: '100%',
-        width: barLength,
-    };
+    componentDidMount() {
+        const {level} = this.props.skill;
+        const barLength = `${level}%`;
+        const skillSection = document.querySelector('#skills');
+        const iObserver = new IntersectionObserver(entries => {
+        console.log("level: ", level);
+        if (!entries[0].isIntersecting) {
+            this.setState({
+                divStyle: {
+                    height: '100%',
+                    width: '0%',
+                    transitionDuration: '2s'
+                }
+            })
+        } else {
+            this.setState({
+                divStyle: {
+                    height: '100%',
+                    width: barLength,
+                    transitionDuration: '2s'
+                }
+            })
+        }
+        });
 
-    return (
-        <div class="skill">
-            <h3>{name}</h3>
-            <div className="skill-meter">
-                <div className="skill-level" style={divStyle}></div>
+        iObserver.observe(skillSection);
+    }
+    
+    render() {
+        console.log("divStyle: ", this.state.divStyle);
+        const {name} = this.props.skill;
+        return (
+            <div class="skill">
+                <h3>{name}</h3>
+                <div className="skill-meter">
+                    <div className="skill-level" style={this.state.divStyle}></div>
+                </div>
             </div>
-        </div>
-        
-    );
+            
+        );
+    }   
 };
 export default Skill;
