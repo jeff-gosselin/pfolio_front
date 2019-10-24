16f1a048b46999cdd090ce18db1107b1a06859f1
jeff-gosselin <jgoz.email@gmail.com>
2019-10-24 12:25:52 -0400

    CORS

--- src/urlBase.js
+++ src/urlBase.js
@@ -1,5 +1,5 @@
-// const url = 'https://jg-pfolio-api.herokuapp.com/';
-// export {url};
-
-const url = 'http://localhost:3000/';
+const url = 'https://jg-pfolio-api.herokuapp.com/';
 export {url};
+
+// const url = 'http://localhost:3000/';
+// export {url};
