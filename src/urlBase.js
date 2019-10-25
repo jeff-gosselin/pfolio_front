efdd80eb93275bf3e5ecd341f355905d7c8dbbd3
jeff-gosselin <jgoz.email@gmail.com>
2019-10-25 17:52:42 -0400

    CORS solution trial

--- src/urlBase.js
+++ src/urlBase.js
@@ -1,4 +1,4 @@
-const url = 'https://jg-pfolio-api.herokuapp.com/';
+const url = 'https://cors-anywhere.herokuapp.com/https://jg-pfolio-api.herokuapp.com/';
 export {url};
 
 // const url = 'http://localhost:3000/';
